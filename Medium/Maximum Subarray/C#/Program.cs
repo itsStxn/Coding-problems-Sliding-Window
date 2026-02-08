929bf7d0739c741dc99c84f6d49d4dc621ac5253
Stanley Ivan Sandjo <heboivan@gmail.com>
2026-02-08 22:32:19 +0100

    Minor code change - Solved - C# - Maximum Subarray

--- Medium/Maximum Subarray/C#/Program.cs
+++ Medium/Maximum Subarray/C#/Program.cs
@@ -1,11 +1,12 @@
 ﻿using Maximum_Subarray;
 
-var task = new Solution();
 
+var task = new Solution();
 Console.WriteLine(task.MaxSubArray(nums: [-2,1,-3,4,-1,2,1,-5,4]));
 Console.WriteLine(task.MaxSubArray(nums: [1]));
 Console.WriteLine(task.MaxSubArray(nums: [5,4,-1,7,8]));
 
-Console.WriteLine(task.MaxSubArray_DivideAndConq(nums: [-2,1,-3,4,-1,2,1,-5,4]));
-Console.WriteLine(task.MaxSubArray_DivideAndConq(nums: [1]));
-Console.WriteLine(task.MaxSubArray_DivideAndConq(nums: [5,4,-1,7,8]));
+task = new AlternativeSolution();
+Console.WriteLine(task.MaxSubArray(nums: [-2,1,-3,4,-1,2,1,-5,4]));
+Console.WriteLine(task.MaxSubArray(nums: [1]));
+Console.WriteLine(task.MaxSubArray(nums: [5,4,-1,7,8]));
--- Medium/Maximum Subarray/C#/Solution.cs
+++ Medium/Maximum Subarray/C#/Solution.cs
@@ -3,7 +3,7 @@ using System;
 namespace Maximum_Subarray;
 
 public class Solution {
-	public int MaxSubArray(int[] nums) {
+	virtual public int MaxSubArray(int[] nums) {
 		int max = int.MinValue;
 		int sum = 0;
 		
@@ -14,36 +14,5 @@ public class Solution {
 		}
 
 		return max;
-	}
-
-	public int MaxSubArray_DivideAndConq(int[] nums) {
-		Func<int, int, int> max = Math.Max;
-		
-		int divideConquer(int left, int right) {
-			if (left == right)
-				return nums[left];
-
-			int mid = left + (right - left) / 2;
-
-			int lSum = int.MinValue, sum = 0;
-			for (int i = mid; i >= left; i--) {
-				sum += nums[i];
-				lSum = max(lSum, sum);
-			}
-
-			int rSum = int.MinValue; sum = 0;
-			for (int i = mid + 1; i <= right; i++) {
-				sum += nums[i];
-				rSum = max(rSum, sum);
-			}
-
-			int mMax = lSum + rSum;
-			int lMax = divideConquer(left, mid);
-			int rMax = divideConquer(mid + 1, right);
-
-			return max(mMax, max(lMax, rMax));
-		}
-		
-		return divideConquer(0, nums.Length - 1);
 	}
 }
